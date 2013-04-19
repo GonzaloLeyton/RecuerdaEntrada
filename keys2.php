e5450e1ac1e54d8f28cba0e975178849d2c57172
Gonzalo <gonzaloleytonf@gmail.com>
2013-04-18 21:10:06 -0300

    Eliminando archivos inservibles

--- keys2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$consumerKey = "JpycBULRNyMR0chhF22JnQ";
-$consumerSecretKey = "weRWszC4DZU8PDdvJ8J7jhQxowrGtK2cDmr9ScU5e9A";
-$oAuthToken = "865784058-KOgUCnWQxwTgaz2Wcu7Kyn33FMdgjXIy0pRaqtxA";
-$oAuthTokenSecret = "RZQgzWCo0zijGIuHIrD48wMEpNfdcd7HzZGs70TJQ";
-?>
